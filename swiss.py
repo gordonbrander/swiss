a7080750edcb4c88c72dea679078709522c94c84
Gordon Brander <gordonbrander@users.noreply.github.com>
2016-12-13 07:05:40 -0800

    Introduce --unit flag
    Finds a grid that will fit on top of an underlying px grid.

--- swiss.py
+++ swiss.py
@@ -18,50 +18,72 @@ calc_parser.add_argument(
 calc_parser.add_argument(
     "width",
     help="Space that grid must fit within",
-    type=int,
+    type=float,
     default=1024
 )
 calc_parser.add_argument(
     "gutter",
     help="Gutter width",
-    type=int,
+    type=float,
     default=24
 )
 calc_parser.add_argument(
-    '-g','--include_gutter',
-    help="Include gutter on the left and right of the grid?",
+    '-o','--include_outer',
+    help="Include outer gutter on the left and right of the grid?",
     type=bool,
     default=False
 )
 calc_parser.add_argument(
-    '-o','--out_file',
-    help="A file to write results to. If no file is provided, stdin will be used.",
-    type=argparse.FileType('w'),
-    default=stdout
+    '-u','--unit',
+    help="""An underlying pixel grid unit to fit grid to (defaults to 1).
+    For example, if you specify 8, the grid generated will cleanly fit on top
+    of an 8px grid.""",
+    type=float,
+    default=1
 )
 
-def fit_col(columns, width, gutter):
+def nearest_divisible(n, x):
+    """
+    Find the nearest number below n that is divisible by x
+    with no fraction remaining
+    """
+    return (n - (n % x)) if n > x else x
+
+def fit_col(columns, width, gutter, unit=1):
     """
     Calculate nearest column width that will fit `columns` number of columns
     within `width` given `gutter`.
+
+    unit (if given) will ensure that the grid fits on top of an underlying
+    pixel grid.
     """
-    return int(floor((width - (gutter * (columns - 1))) / columns))
+    span = (width - (gutter * (columns - 1))) / columns
+    return nearest_divisible(span, unit)
+
+def fit_gutter(gutter, unit=1):
+    """Find nearest gutter that will fit on top of grid of `unit`"""
+    return nearest_divisible(gutter, unit)
 
 def colspan(span, column, gutter):
-    return int((column * span) + (gutter * (span - 1)))
+    """
+    Calculate colspan for `span` number of columns given `column` and `gutter`
+    width.
+    """
+    return (column * span) + (gutter * (span - 1))
 
 def swiss_calc():
     """Calc command"""
     args = calc_parser.parse_args()
     columns = args.columns
-    gutter = args.gutter
-    include_gutter = args.include_gutter
-    width = (args.width - (gutter * 2)) if include_gutter else args.width
+    unit = args.unit
+    gutter = fit_gutter(args.gutter, unit)
+    include_outer = args.include_outer
+    width = (args.width - (gutter * 2)) if include_outer else args.width
     column = fit_col(columns, width, gutter)
     print("Width: {}".format(colspan(columns, column, gutter)))
     print("Gutter: {}".format(gutter))
-    for i in range(1, columns):
-        print("Column {}x: {}".format(i, colspan(i, column, gutter)))
+    for i in range(0, columns):
+        print("Colspan {}: {}".format(i + 1, colspan(i + 1, column, gutter)))
 
 if __name__ == '__main__':
     swiss_calc()
